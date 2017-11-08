7a788749b6405ce8f923760e414f0e6f0c5cb85f
Ian Philip Eglin <philip@eglin.no>
2017-11-08 10:44:16 +0100

    Trying to integrate calculator into socket

--- Server/metServer.py
+++ Server/metServer.py
@@ -2,6 +2,10 @@ import socket
 import sys
 import time
 
+def space():
+    print()
+    print()
+
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -36,6 +40,15 @@ try:
 
     dataRes = float(dChar[2] + dChar[3] + dChar[4] + dChar[5] + dChar[6] + dChar[7])
     print(dataRes)
+
+
+    R = float(input("R = %s" + str(chr(13))) % dataRes)
+    t = ((((-R_0)*a) + math.sqrt((R_0 ** 2) * (a ** 2) - 4 * R_0 * b * (R_0 - R))) / (2 * R_0 * b))
+    space()
+    if(t == -0.0):
+        print("t = 0 C")
+    else:
+        print("t =",t,"C")
 
 
 finally:
