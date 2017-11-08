0e90e5dfcf7b24adc9136ac9b6b9ec39b8953355
Ian Philip Eglin <philip@eglin.no>
2017-11-08 11:16:26 +0100

    Implementing rounding to two decimals for temperature

--- Server/metServer.py
+++ Server/metServer.py
@@ -51,9 +51,10 @@ try:
     R = float(dataRes)
     t = ((((-R_0)*a) + math.sqrt((R_0 ** 2) * (a ** 2) - 4 * R_0 * b * (R_0 - R))) / (2 * R_0 * b))
     if(t == -0.0):
-        print(int(0))
+        print(int(0.0))
     else:
-        print(t)
+        currentTemp = ("%.2f" % t)
+        print(currentTemp)
 
 
 finally:
