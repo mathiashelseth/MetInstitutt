fc33b2891b8d7f774ba12c593e62f1f1d3472049
Ian Philip Eglin <philip@eglin.no>
2017-11-30 17:46:55 +0100

    Adding a try/except to prevent OSErrors such as WinError

--- Server/metServer.py
+++ Server/metServer.py
@@ -42,105 +42,112 @@ def systemReboot():
 # Main function for sending and receiving data
 def main():
 
-    # Setting an alternative timer to run code every 5 seconds
-    threading.Timer(5.0, main).start()
-
-    # Setting timer to force script reboot every 2 hours to prevent WinError 10060
-    threading.Timer(300.0, systemReboot).start()
-
+    #Try to run main function
     try:
 
-        # Send data
-        message = "#04" + str(chr(13))
-        #print('Sending "%s"' % message)
-        sock.send(message.encode('utf-8'))
+        # Setting a timer to run function every 5 seconds
+        threading.Timer(5.0, main).start()
 
-        # Look for the response
-        amount_received = 0
-        amount_expected = 9
+        # Setting timer to force script reboot every 2 hours to prevent WinError 10060
+        threading.Timer(300.0, systemReboot).start()
 
-        # Splitting characters from received data so only numbers remain
-        dChar = []
-        while amount_received < amount_expected:
-            data = sock.recv(1)
-            amount_received += len(data)
-            dataFull = str(data,'utf-8')
-
-            dVal = dataFull
-            dChar.append(dVal)
-
-        # Collecting specific integers from reveived data
-        dataRes = float(dChar[2] + dChar[3] + dChar[4] + dChar[5] + dChar[6] + dChar[7])
-
-        # Formula for finding temperature using a measurement of ohm
-        R = float(dataRes)
-        t = ((((-R_0)*a) + math.sqrt((R_0 ** 2) * (a ** 2) - 4 * R_0 * b * (R_0 - R))) / (2 * R_0 * b))
-        if(t == -0.0):
-
-            # Debugging given value for temperatur when provided ohm is 100.00
-            currentTemp = 0.0
-        else:
-
-            # Rounding off to a two decimal number
-            currentTemp = ("%.2f" % t)
-
-        # Sending all measurements to array
-        minuteMeasurements.append(float(currentTemp))
-
-        # Checking if length of array is 12 or more
-        if(len(minuteMeasurements) >= 12):
-
-            # Adding timestamp to average temperature per minute|
-            timestamp = '{:%d.%m.%Y  %H:%M:%S}'.format(datetime.datetime.now())
-
-            # Finding temp average per minute
-            avrgMeasurement = sum(minuteMeasurements) / len(minuteMeasurements)
-
-            # Rounding off average temperature to two decimal
-            avrgTemp = float("%.2f" % avrgMeasurement)
-
-            # Prepare SQL query to INSERT a record into the database.
-            sql = "INSERT INTO data_main_min(TIMESTAMP, OHM, CELSIUS, UPTIME, DOWNTIME) \
-                     VALUES ('%s', '%f', '%f', '%i', '%i')" % \
-                     (timestamp, R, avrgTemp, 1, 0)
-            try:
-               # Execute the SQL command
-               cursor.execute(sql)
-               # Commit your changes in the database
-               db.commit()
-               print("Data is sent to database")
-            except:
-               # Rollback in case there is any error
-               db.rollback()
-               print("Database Error: Not sending data to database")
-
-            # Print result to console
-            print(timestamp + "   " + str(avrgTemp))
-
-            # Clearing array for measurements for next minute
-            minuteMeasurements.clear()
-        else:
-
-            # If array is not longer than or equal to 12: ignore this is/else statement
-            pass
-
-        # Wait 5 seconds before restarting function
-        time.sleep(5)
-
-    # In case of error while sending or receiving data, try closing socket an rebooting
-    except:
-
-        # Closing the socket
-        sock.close()
         try:
 
-            # Trying to restart the main function without shutting down
-            main()
+            # Send data
+            message = "#04" + str(chr(13))
+            #print('Sending "%s"' % message)
+            sock.send(message.encode('utf-8'))
+
+            # Look for the response
+            amount_received = 0
+            amount_expected = 9
+
+            # Splitting characters from received data so only numbers remain
+            dChar = []
+            while amount_received < amount_expected:
+                data = sock.recv(1)
+                amount_received += len(data)
+                dataFull = str(data,'utf-8')
+
+                dVal = dataFull
+                dChar.append(dVal)
+
+            # Collecting specific integers from reveived data
+            dataRes = float(dChar[2] + dChar[3] + dChar[4] + dChar[5] + dChar[6] + dChar[7])
+
+            # Formula for finding temperature using a measurement of ohm
+            R = float(dataRes)
+            t = ((((-R_0)*a) + math.sqrt((R_0 ** 2) * (a ** 2) - 4 * R_0 * b * (R_0 - R))) / (2 * R_0 * b))
+            if(t == -0.0):
+
+                # Debugging given value for temperatur when provided ohm is 100.00
+                currentTemp = 0.0
+            else:
+
+                # Rounding off to a two decimal number
+                currentTemp = ("%.2f" % t)
+
+            # Sending all measurements to array
+            minuteMeasurements.append(float(currentTemp))
+
+            # Checking if length of array is 12 or more
+            if(len(minuteMeasurements) >= 12):
+
+                # Adding timestamp to average temperature per minute|
+                timestamp = '{:%d.%m.%Y  %H:%M:%S}'.format(datetime.datetime.now())
+
+                # Finding temp average per minute
+                avrgMeasurement = sum(minuteMeasurements) / len(minuteMeasurements)
+
+                # Rounding off average temperature to two decimal
+                avrgTemp = float("%.2f" % avrgMeasurement)
+
+                # Prepare SQL query to INSERT a record into the database.
+                sql = "INSERT INTO data_main_min(TIMESTAMP, OHM, CELSIUS, UPTIME, DOWNTIME) \
+                         VALUES ('%s', '%f', '%f', '%i', '%i')" % \
+                         (timestamp, R, avrgTemp, 1, 0)
+                try:
+                   # Execute the SQL command
+                   cursor.execute(sql)
+                   # Commit your changes in the database
+                   db.commit()
+                   print("Data is sent to database")
+                except:
+                   # Rollback in case there is any error
+                   db.rollback()
+                   print("Database Error: Not sending data to database")
+
+                # Print result to console
+                print(timestamp + "   " + str(avrgTemp))
+
+                # Clearing array for measurements for next minute
+                minuteMeasurements.clear()
+            else:
+
+                # If array is not longer than or equal to 12: ignore this is/else statement
+                pass
+
+            # Wait 5 seconds before restarting function
+            time.sleep(5)
+
+        # In case of error while sending or receiving data, try closing socket an rebooting
         except:
 
-            # Rebooting program when program fails
-            systemReboot()
+            # Closing the socket
+            sock.close()
+            try:
 
+                # Trying to restart the main function without shutting down
+                main()
+            except:
+
+                # Rebooting program when program fails
+                systemReboot()
+
+    except OSError:
+
+        # Rebooting program if WinError
+        systemReboot()
 
 
 #------------------------Start of program------------------------#
