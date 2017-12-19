ba8418974d1da04785cece304aab8742f62da162
Mathias Helseth <mathiashelseth99@gmail.com>
2017-12-19 15:30:04 +0100

    Removed annoying echo

--- Server/check_server.bat
+++ Server/check_server.bat
@@ -3,7 +3,6 @@
 tasklist /FI "IMAGENAME eq python.exe" 2>NUL | find /I /N "python.exe">NUL
 
 if "%ERRORLEVEL%"=="0" (
-    echo The program is running
     goto start
 ) ELSE (
     echo The program is not running
